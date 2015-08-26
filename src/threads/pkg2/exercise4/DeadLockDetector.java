c4abb3ff67455390993774e6dc0f8a76f3bf89bd
Alexander Steen <AlexanderSteen@Alexanders-MacBook-Pro.local>
2015-08-26 14:47:28 +0200

    Added exercise files

---
+++ src/threads/pkg2/exercise4/DeadLockDetector.java
@@ -0,0 +1,25 @@
+package threads.pkg2.exercise4;
+
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadMXBean;
+
+/**
+ * @author Lars Mortensen
+ */
+class DeadLockDetector implements Runnable {
+
+  ThreadMXBean tmxb = ManagementFactory.getThreadMXBean();
+  boolean doRun = true;
+  
+  public void stop() {
+    this.doRun = false;
+  }
+  
+  @Override
+  public void run() {
+    while (doRun) {
+      long[] threadIds = tmxb.findDeadlockedThreads();
+      //...
+    }
+  }
+}
